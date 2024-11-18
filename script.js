f1ea605c1243785c01ebe124e3103cd6b36bbced
lapse2l <srk9491653011@gmail.com>
2024-11-18 22:44:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+document.querySelectorAll("button")[0].addEventListener("click", a);
+document.querySelectorAll("button")[1].addEventListener("click", b);
+document.querySelectorAll("button")[2].addEventListener("click", c);
+document.querySelectorAll("button")[3].addEventListener("click", d);
+document.querySelectorAll("button")[4].addEventListener("click", e);
+document.querySelectorAll("button")[5].addEventListener("click", f);
+document.querySelectorAll("button")[6].addEventListener("click", g);
+function a()
+{
+    alert("I Love You ");
+} 
+function b()
+{
+    alert("I Miss You ");
+} 
+function c()
+{
+    alert("I Love You So much ");
+} 
+function d()
+{
+    alert("I Hug YOu");
+} 
+function e()
+{
+    alert("I Hate you coz love is increasing  ");
+} 
+function f()
+{
+    alert("I Love You alot ammu ");
+} 
+function g()
+{
+    alert("I Love You Ammshiv ");
+} 
